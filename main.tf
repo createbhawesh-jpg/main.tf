d5f96e20cf70ed8bb7fe16b8a98c7aa43a3b17a2
Bhawesh Kumar <createbhawesh@gmail.com>
2026-05-26 23:48:44 +0530

    Update main.tf
    resource "azurerm_storage_account" "mukeshbhai12" {
      name                     = "digamberbhai12"
      resource_group_name      = azurerm_resource_group.manojbhai12.name
      location                 = azurerm_resource_group.manojbhai12.location
      account_replication_type = "LRS"
      account_tier             = "Standard"
    }
    resource "azurerm_storage_container" "genuva" {
      name                  = "huganda1"
      storage_account_id    = azurerm_storage_account.mukeshbhai12.id
      container_access_type = "private"
    }

--- main.tf
+++ main.tf
@@ -2,4 +2,15 @@ resource "azurerm_resource_group" "manojbhai12" {
   name     = "aditya123"
   location = "westus"
 }
-
+resource "azurerm_storage_account" "mukeshbhai12" {
+  name                     = "digamberbhai12"
+  resource_group_name      = azurerm_resource_group.manojbhai12.name
+  location                 = azurerm_resource_group.manojbhai12.location
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
+}
+resource "azurerm_storage_container" "genuva" {
+  name                  = "huganda1"
+  storage_account_id    = azurerm_storage_account.mukeshbhai12.id
+  container_access_type = "private"
+}
